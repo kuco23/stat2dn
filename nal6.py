c9c321fff9d92109b5e0b2087734026fe7217e55
kuco23 <nseverkar@gmail.com>
2021-08-30 20:25:31 +0200

    nal7

--- nal6.py
+++ nal6.py
@@ -1,5 +1,6 @@
 from operator import truediv
 import math
+from itertools import product
 import numpy as np
 from scipy.linalg import lstsq, schur
 from scipy.stats import f as F
@@ -7,6 +8,8 @@ from scipy.stats import t as T
 from scipy.spatial import Delaunay
 import matplotlib.pyplot as plt
 import matplotlib.tri as mtri
+from matplotlib import cm
+from matplotlib.colors import LightSource
 from mpl_toolkits.mplot3d import Axes3D
 
 def getData(path):
@@ -22,6 +25,16 @@ def getData(path):
             else:
                 data.append(list(map(float, sline)))
     return (names, data)
+
+def linearRegresh(path, const=True):
+    names, data = getData(path)
+    data = np.asarray(data)
+    X = data[:,:-1]
+    y = data[:, -1]
+    if const:
+        X = np.insert(X, 0, values=np.ones(len(y)), axis=1)
+    b = lstsq(X,y)[0]
+    return (X, y, b)
 
 def FTestConstants(X,y,b,alpha):
     m, n = X.shape[1]-1, len(y)
@@ -45,17 +58,7 @@ def TTestConstants(X,y,b,alpha):
     Talpha2 = T.ppf(1-alpha/2, n-m-1)
     return (Tstat, Talpha2)
 
-def linearRegresh(path, const=True):
-    names, data = getData(path)
-    data = np.asarray(data)
-    X = data[:,:-1]
-    y = data[:, -1]
-    if const:
-        X = np.insert(X, 0, values=np.ones(len(y)), axis=1)
-    b = lstsq(X,y)[0]
-    return (X, y, b)
-
-def getConfidenceElipsoid(X, y, b):
+def getConfidenceElipsoidData(X, y, b):
     n, m = X.shape
     XtX = np.matmul(np.transpose(X),X)
     S, Q = schur(XtX)
@@ -65,56 +68,7 @@ def getConfidenceElipsoid(X, y, b):
     L = np.diagonal(S)
     return (Q, L, r)
 
-def drawElipsoid(A, b, r, k=30):
-    sqrtr = math.sqrt(r)
-
-    # get data for a sphere
-    T1 = np.linspace(0, 2 * np.pi, k)
-    T2 = np.linspace(0, 2 * np.pi, k)
-    X = np.zeros((k,k))
-    Y = np.zeros((k,k))
-    Z = np.zeros((k,k))
-    for i in range(k):
-        for j in range(k):
-            X[i,j] = np.cos(T1[i]) * np.sin(T2[j])
-            Y[i,j] = np.sin(T1[i]) * np.sin(T2[j])
-            Z[i,j] = np.cos(T2[i])
-
-    # construct the elipsoid
-    U1 = np.zeros((k,k))
-    U2 = np.zeros((k,k))
-    U3 = np.zeros((k,k))
-    for i in range(k):
-        for j in range(k):
-            xyz = np.array([X[i,j], Y[i,j], Z[i,j]])
-            u = b + np.matmul(A, sqrtr * xyz)
-            [U1[i,j], U2[i,j], U3[i,j]] = u
-
-    # triangulate
-    [T1, T2] = np.meshgrid(T1, T2)
-    X,Y,Z = X.flatten(), Y.flatten(), Z.flatten()
-    tri = Delaunay(np.array([X, Y, Z]).T)
-    print(tri.simplices)
-    
-    # plot the elipsoid
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.plot_trisurf(
-        X, Y, Z,
-        triangles=tri.simplices
-    )
-    plt.show()
-
-def testIfInInterval(X, y, b, Q, L, b0):
-    (n, m) = X.shape
-    L = np.diagflat(np.sqrt(L))
-    vkr = np.linalg.norm(y - X @ b)**2
-    Falpha = F.ppf(1-alpha, m, n-m)
-    v1 = np.linalg.norm((L @ Q.T) @ (b - b0))**2
-    v2 = m * vkr * Falpha / (n - m)
-    return (v1, v2)
-
-def getBonferi(X,y,b,alpha):
+def getBonferiCorrection(X,y,b,alpha):
     m, n = X.shape[1], len(y)
     I = []
     for j in range(m):
@@ -126,6 +80,77 @@ def getBonferi(X,y,b,alpha):
         Tstat = Talpha2 * vkr * math.sqrt(invXtX00 / (n-m))
         I.append((b[j] - Tstat, b[j] + Tstat))
     return I
+
+def drawElipsoid(ax, A, b, k=30):
+    # sphere parametrization
+    T = np.linspace(0, 2 * np.pi, k)
+    X = np.zeros((k,k))
+    Y = np.zeros((k,k))
+    Z = np.zeros((k,k))
+    for i in range(k):
+        for j in range(k):
+            X[i,j] = np.cos(T[i]) * np.sin(T[j])
+            Y[i,j] = np.sin(T[i]) * np.sin(T[j])
+            Z[i,j] = np.cos(T[j])
+
+    # map sphere to elipsoid
+    U1 = np.zeros((k,k))
+    U2 = np.zeros((k,k))
+    U3 = np.zeros((k,k))
+    for i in range(k):
+        for j in range(k):
+            xyz = np.array([X[i,j], Y[i,j], Z[i,j]])
+            [U1[i,j], U2[i,j], U3[i,j]] = b + A @ xyz
+
+    # triangulate the elipsoid
+    [T1, T2] = np.meshgrid(T, T)
+    tri = Delaunay(np.array([T1.flatten(), T2.flatten()]).T)
+    
+    # plot the elipsoid
+    ax.plot_trisurf(
+        U1.flatten(), U2.flatten(), U3.flatten(),
+        triangles=tri.simplices, cmap=cm.inferno,
+        lightsource = LightSource()
+    )
+    return ax
+
+def drawCuboid(ax, I):
+    # get centers and sizes from interval list
+    center = [(a+b)/2 for (a,b) in I]
+    size = [b - a for (a,b) in I]
+
+    # do some magic
+    o = [a - b / 2 for a, b in zip(center, size)]
+    l, w, h = size
+    x = np.array([[o[0], o[0] + l, o[0] + l, o[0], o[0]],
+         [o[0], o[0] + l, o[0] + l, o[0], o[0]],  
+         [o[0], o[0] + l, o[0] + l, o[0], o[0]],  
+         [o[0], o[0] + l, o[0] + l, o[0], o[0]]])
+    y = np.array([[o[1], o[1], o[1] + w, o[1] + w, o[1]],
+         [o[1], o[1], o[1] + w, o[1] + w, o[1]],  
+         [o[1], o[1], o[1], o[1], o[1]],          
+         [o[1] + w, o[1] + w, o[1] + w, o[1] + w, o[1] + w]])
+    z = np.array([[o[2], o[2], o[2], o[2], o[2]],                        
+         [o[2] + h, o[2] + h, o[2] + h, o[2] + h, o[2] + h],    
+         [o[2], o[2], o[2] + h, o[2] + h, o[2]],                
+         [o[2], o[2], o[2] + h, o[2] + h, o[2]]])
+
+    # plot cuboid
+    ax.plot_surface(
+        x, y, z,
+        cmap=cm.bone, alpha=0.2,
+        lightsource = LightSource()
+    )
+    return ax
+
+def testIfInInterval(X, y, b, Q, L, b0):
+    (n, m) = X.shape
+    L = np.diagflat(np.sqrt(L))
+    vkr = np.linalg.norm(y - X @ b)**2
+    Falpha = F.ppf(1-alpha, m, n-m)
+    v1 = np.linalg.norm((L @ Q.T) @ (b - b0))**2
+    v2 = m * vkr * Falpha / (n - m)
+    return (v1, v2)
         
 
 if __name__ == '__main__':
@@ -133,10 +158,12 @@ if __name__ == '__main__':
     path = 'podatki_6.txt'
     
     X, y, b = linearRegresh(path, False)
-    #Q, L, r = getConfidenceElipsoid(X,y,b)
-    #drawElipsoid(np.matmul(Q, np.diagflat(np.power(L, -1/2))), b, math.sqrt(r))
+    Q, L, r = getConfidenceElipsoidData(X,y,b)
+    A = math.sqrt(r) * Q @ np.diagflat(np.power(L, -1/2))
+    I = getBonferiCorrection(X, y, b, alpha)
 
-    #print(testIfInInterval(X, y, b, Q, L, np.array([1, -1, -0.45])))
-        
-    I = getBonferi(X, y, b, alpha)
+    ax = plt.figure().gca(projection='3d')
+    ax = drawElipsoid(ax, A, b, 40)
+    ax = drawCuboid(ax, I)
+    plt.show()
 
